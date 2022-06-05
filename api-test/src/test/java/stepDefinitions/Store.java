c1e54fe85f162a79faff7aaff73c5782bf6fff47
davidsusantoid <ddavid0505@gmail.com>
2022-06-06 05:39:31 +0700

    commit

---
+++ api-test/src/test/java/stepDefinitions/Store.java
@@ -0,0 +1,10 @@
+package stepDefinitions;
+
+import io.restassured.response.Response;
+
+public class Store {
+
+    private static final String BASE_URL = "https://petstore3.swagger.io/api/v3";
+
+    private static Response response;
+}
---
+++ api-test/src/test/java/stepDefinitions/User.java
@@ -0,0 +1,10 @@
+package stepDefinitions;
+
+import io.restassured.response.Response;
+
+public class User {
+
+    private static final String BASE_URL = "https://petstore3.swagger.io/api/v3";
+
+    private static Response response;
+}
